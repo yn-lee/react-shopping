b9e0241977eb33d2689ab29f4a74eb0fe68ad845
yn-lee <message1315@gmail.com>
2021-06-17 20:35:35 +0900

    기본 homepage 추가

---
+++ src/components/directory/directory.component.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+import MenuItem from "../menu-item/menu-item.component";
+
+import section from "./directory.data";
+
+import "./directory.styles.scss";
+
+class Directory extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sections: section
+    };
+  }
+
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map((section) => (
+          <MenuItem
+            key={section.div}
+            title={section.title}
+            imgUrl={section.imageUrl}
+            size={section.size}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Directory;
